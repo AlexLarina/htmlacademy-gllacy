24aa680847f781ea24b213f82589bf1f66ce189d
alex_larina <aa.larina.ff@gmail.com>
2020-01-29 12:38:49 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Личный проект «Глейси»
+
+Проект создан на базе [htmlacademy](https://htmlacademy.ru/) для учебных целей
+
+* Студент: [Александра Ларина](https://up.htmlacademy.ru/htmlcss/27/user/422049).
+
+---
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+const browsersync = require("browser-sync").create();
+
+function browserSync() {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+}
+
+function browserSyncReload() {
+  browsersync.reload();
+}
+
+function watchFiles() {
+  gulp.watch("css/style.css").on('change', browserSyncReload);
+  gulp.watch("index.html").on('change', browserSyncReload);
+}
+
+const watch = gulp.parallel(watchFiles, browserSync);
+watch();
